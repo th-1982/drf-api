b7a8ccceb68fc3830b4085efefc22f79284d77b6
Theresa Wolff <theresawolff4@gmail.com>
2024-01-05 22:47:39 +0000

    Added PostList view, list and create a post

--- posts/serializers.py
+++ posts/serializers.py
@@ -31,5 +31,5 @@ class PostSerializer(serializers.ModelSerializer):
         fields = [
             'id', 'owner', 'is_owner', 'profile_id',
             'profile_image', 'created_at', 'updated_at',
-            'title', 'content', 'image' 'image_filter'
+            'title', 'content', 'image', 'image_filter'
         ]
