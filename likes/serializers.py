dc34f8133b1cf272ed7e7065c5cc8288f338d8b8
Theresa Wolff <theresawolff4@gmail.com>
2024-01-07 01:24:44 +0000

    Created LikeList and LikeDetail generic

--- likes/serializers.py
+++ likes/serializers.py
@@ -13,8 +13,8 @@ class LikeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Like
         fields = ['id', 'created_at', 'owner', 'post']
-    
-     def create(self, validated_data):
+
+    def create(self, validated_data):
         try:
             return super().create(validated_data)
         except IntegrityError:
